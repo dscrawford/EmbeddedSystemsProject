488e35123d74b40d6e31d90f139173a5459b2a13
Dumonu <jacobcherry36@gmail.com>
2019-09-17 13:01:38 -0500

    Added switch commands

--- ass_2/src/command.c
+++ ass_2/src/command.c
@@ -1,1 +1,42 @@
 #include "command.h"
+
+// Switch Commands
+bool throwSwitchThrough(uint8_t addr)
+{
+    if(addr > 0x7F) return false; // 7-bit address
+    uint8_t command = 0; // 00
+    uint8_t data = 0x00; // 00000
+    return buildCommand(SWITCH, addr, command, data);
+}
+
+bool throwSwitchOut(uint8_t addr)
+{
+    if(addr > 0x7F) return false; // 7-bit address
+    uint8_t command = 0; // 00
+    uint8_t data = 0x1F; // 11111
+    return buildCommand(SWITCH, addr, command, data);
+}
+
+bool setSwitchAddr(uint8_t addr)
+{
+    if(addr > 0x7F) return false; // 7-bit address
+    uint8_t command = 1; // 01
+    uint8_t data = 0x0B; // 01011
+    return buildCommand(SWITCH, addr, command, data);
+}
+
+bool assignSwitchToRouteThrough(uint8_t addr, uint8_t rtaddr)
+{
+    if(addr > 0x7F) return false; // 7-bit address
+    if(rtaddr > 0x1F) return false; // 5-bit data
+    uint8_t command = 2; // 10
+    return buildCommand(SWITCH, addr, command, rtaddr);
+}
+
+bool assignSwitchToRouteOut(uint8_t addr, uint8_t rtaddr)
+{
+    if(addr > 0x7F) return false; // 7-bit address
+    if(rtaddr > 0x1F) return false; // 5-bit data
+    uint8_t command = 3; // 11
+    return buildCommand(SWITCH, addr, command, rtaddr);
+}
--- ass_2/src/command.h
+++ ass_2/src/command.h
@@ -1,10 +1,18 @@
 #ifndef COMMAND_H_20190917_111805
 #define COMMAND_H_20190917_111805
 
-typedef bool uint8fastest_t;
+#include <stdint.h>
+
+typedef bool uint_fast8_t;
 #define true 1
 #define false 0
 
+// Switch Commands
+bool throwSwitchThrough(uint8_t addr);
+bool throwSwitchOut(uint8_t addr);
+bool setSwitchAddr(uint8_t addr); // I'm not sure what this command accomplishes
+bool assignSwitchToRouteThrough(uint8_t addr, uint8_t rtaddr);
+bool assignSwitchToRouteOut(uint8_t addr, uint8_t rtaddr);
 
 #endif /* COMMAND_H_20190917_111805 */
 
