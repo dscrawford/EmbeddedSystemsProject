dfbc1687c3d89341fc031a5d2fa5a025223efdb0
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2019-11-21 12:43:10 -0600

    Some semblance of an interface

--- ass_3/ass_3_bgin.py
+++ ass_3/ass_3_bgin.py
@@ -2,57 +2,52 @@
 
 import curses
 
-# from move_direction.py import *
-
-def forward ():
-    stdscr.addstr("forward\n")
-
-def left ():
-    stdscr.addstr("left\n")
-
-def back ():
-    stdscr.addstr("back\n")
-
-def right ():
-    stdscr.addstr("right\n")
+from motor.py import *
 
 stdscr = curses.initscr()
+output = curses.newwin(curses.LINES - 4, curses.COLS, 0, 0)
+commands = curses.newwin(4, curses.COLS, curses.LINES - 4, 0)
 curses.nl(True)
 curses.raw()
 curses.noecho()
 curses.curs_set(False)
+output.scrollok(True)
+output.move(0, 0)
+stdscr.refresh()
 
-y = 0
+commands.border(curses.ACS_VLINE, curses.ACS_VLINE, curses.ACS_HLINE, curses.ACS_HLINE,
+    curses.ACS_ULCORNER, curses.ACS_URCORNER, curses.ACS_LLCORNER, curses.ACS_LRCORNER)
+inc = int((curses.COLS - 2) / 4)
+x = 1
+commands.addstr(2, x, "[WASD]: direction")
+x += inc
+commands.addstr(2, x, "[Space]: stop")
+x += inc
+commands.addstr(2, x, "[Q]: quit")
+x += inc
+commands.addstr(2, x, "[M]: enable manual control")
+
 key = 0
+manual = False
 while key != ord('q'):
+    commands.addstr(1, 1, "Manual control: %s" % ("on " if manual else "off"))
+
+    output.refresh()
+    commands.refresh()
+
     key = stdscr.getch()
 
-    stdscr.move(y, 0)
-    if key == ord('w') or key == ord('W'):
+    if key == ord('m') or key == ord('M'):
+        manual = not manual
+    elif manual and (key == ord('w') or key == ord('W')):
         forward()
-        if y + 1 == curses.LINES:
-            stdscr.scroll()
-        else:
-            y += 1
-    elif key == ord('a') or key == ord('A'):
-        left()
-        if y + 1 == curses.LINES:
-            stdscr.scroll()
-        else:
-            y += 1
-    elif key == ord('s') or key == ord('S'):
+    elif manual and (key == ord('a') or key == ord('A')):
+        left(90)
+    elif manual and (key == ord('s') or key == ord('S')):
         back()
-        if y + 1 == curses.LINES:
-            stdscr.scroll()
-        else:
-            y += 1
-    elif key == ord('d') or key == ord('D'):
-        right()
-        if y + 1 == curses.LINES:
-            stdscr.scroll()
-        else:
-            y += 1
-
-    stdscr.refresh()
+    elif manual and (key == ord('d') or key == ord('D')):
+        right(90)
+    elif key == ord(' '):
+        stop()
 
 curses.endwin()
